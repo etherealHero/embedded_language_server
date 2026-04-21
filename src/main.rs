9905d9ce5037e259b6114c0b5509a33d0d63edda
etherealhero <gutcan4312@gmail.com>
2026-04-21 16:34:18 +1000

    remove example cases

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,33 @@
 use std::ops::ControlFlow;
-use std::time::Duration;
 
 use async_lsp::ClientSocket;
 use async_lsp::client_monitor::ClientProcessMonitorLayer;
 use async_lsp::concurrency::ConcurrencyLayer;
 use async_lsp::lsp_types::{
-    Hover, HoverContents, HoverProviderCapability, InitializeResult, MarkedString, MessageType,
-    OneOf, ServerCapabilities, ShowMessageParams, notification, request,
+    Hover, HoverContents, HoverProviderCapability, InitializeResult, MarkedString, OneOf,
+    ServerCapabilities, notification, request,
 };
 use async_lsp::panic::CatchUnwindLayer;
 use async_lsp::router::Router;
 use async_lsp::server::LifecycleLayer;
 use async_lsp::tracing::TracingLayer;
 use tower::ServiceBuilder;
-use tracing::{Level, info};
+use tracing::Level;
 
 struct ServerState {
-    client: ClientSocket,
+    _client: ClientSocket,
     counter: i32,
 }
-
-struct TickEvent;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let (server, _) = async_lsp::MainLoop::new_server(|client| {
-        tokio::spawn({
-            let client = client.clone();
-            async move {
-                let mut interval = tokio::time::interval(Duration::from_secs(1));
-                loop {
-                    interval.tick().await;
-                    if client.emit(TickEvent).is_err() {
-                        break;
-                    }
-                }
-            }
-        });
-
         let mut router = Router::new(ServerState {
-            client: client.clone(),
+            _client: client.clone(),
             counter: 0,
         });
         router
-            .request::<request::Initialize, _>(|_, params| async move {
-                eprintln!("Initialize with {params:?}");
+            .request::<request::Initialize, _>(|_, _| async move {
                 Ok(InitializeResult {
                     capabilities: ServerCapabilities {
                         hover_provider: Some(HoverProviderCapability::Simple(true)),
@@ -55,16 +38,8 @@ async fn main() {
                 })
             })
             .request::<request::HoverRequest, _>(|st, _| {
-                let client = st.client.clone();
                 let counter = st.counter;
                 async move {
-                    tokio::time::sleep(Duration::from_secs(1)).await;
-                    client
-                        .notify::<notification::ShowMessage>(ShowMessageParams {
-                            typ: MessageType::INFO,
-                            message: "Hello LSP".into(),
-                        })
-                        .unwrap();
                     Ok(Some(Hover {
                         contents: HoverContents::Scalar(MarkedString::String(format!(
                             "I am a hover text {counter}!"
@@ -80,12 +55,7 @@ async fn main() {
             .notification::<notification::DidChangeConfiguration>(|_, _| ControlFlow::Continue(()))
             .notification::<notification::DidOpenTextDocument>(|_, _| ControlFlow::Continue(()))
             .notification::<notification::DidChangeTextDocument>(|_, _| ControlFlow::Continue(()))
-            .notification::<notification::DidCloseTextDocument>(|_, _| ControlFlow::Continue(()))
-            .event::<TickEvent>(|st, _| {
-                info!("tick");
-                st.counter += 1;
-                ControlFlow::Continue(())
-            });
+            .notification::<notification::DidCloseTextDocument>(|_, _| ControlFlow::Continue(()));
 
         ServiceBuilder::new()
             .layer(TracingLayer::default())
