4c5ebf830194d688d6e6d4f8757b6327fb37e7ac
etherealhero <gutcan4312@gmail.com>
2026-04-21 17:10:01 +1000

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,51 @@
-use std::ops::ControlFlow;
-
-use async_lsp::ClientSocket;
-use async_lsp::client_monitor::ClientProcessMonitorLayer;
-use async_lsp::concurrency::ConcurrencyLayer;
-use async_lsp::lsp_types::{
-    Hover, HoverContents, HoverProviderCapability, InitializeResult, MarkedString, OneOf,
-    ServerCapabilities, notification, request,
-};
-use async_lsp::panic::CatchUnwindLayer;
-use async_lsp::router::Router;
-use async_lsp::server::LifecycleLayer;
-use async_lsp::tracing::TracingLayer;
-use tower::ServiceBuilder;
-use tracing::Level;
+use async_lsp::lsp_types::{self as lsp, notification as N, request as R};
 
 struct ServerState {
-    _client: ClientSocket,
+    _client: async_lsp::ClientSocket,
     counter: i32,
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let (server, _) = async_lsp::MainLoop::new_server(|client| {
-        let mut router = Router::new(ServerState {
+        let mut router = async_lsp::router::Router::new(ServerState {
             _client: client.clone(),
             counter: 0,
         });
         router
-            .request::<request::Initialize, _>(|_, _| async move {
+            .request::<R::Initialize, _>(|_, _| async move {
                 tracing::info!("{} v{}", clap::crate_name!(), clap::crate_version!());
-                Ok(InitializeResult {
-                    capabilities: ServerCapabilities {
-                        hover_provider: Some(HoverProviderCapability::Simple(true)),
-                        definition_provider: Some(OneOf::Left(true)),
-                        ..ServerCapabilities::default()
+                Ok(lsp::InitializeResult {
+                    capabilities: lsp::ServerCapabilities {
+                        hover_provider: Some(lsp::HoverProviderCapability::Simple(true)),
+                        definition_provider: Some(lsp::OneOf::Left(true)),
+                        ..lsp::ServerCapabilities::default()
                     },
-                    server_info: None,
+                    server_info: Some(async_lsp::lsp_types::ServerInfo {
+                        name: clap::crate_name!().to_string(),
+                        version: Some(clap::crate_version!().to_string()),
+                    }),
                 })
             })
-            .request::<request::HoverRequest, _>(|st, _| {
+            .request::<R::HoverRequest, _>(|st, _| {
                 let counter = st.counter;
                 async move {
-                    Ok(Some(Hover {
-                        contents: HoverContents::Scalar(MarkedString::String(format!(
-                            "I am a hover text {counter}!"
+                    Ok(Some(lsp::Hover {
+                        contents: lsp::HoverContents::Scalar(lsp::MarkedString::String(format!(
+                            "I am a hover text {counter}!",
                         ))),
                         range: None,
                     }))
                 }
             })
-            .notification::<notification::Initialized>(|_, _| ControlFlow::Continue(()))
-            .notification::<notification::DidChangeConfiguration>(|_, _| ControlFlow::Continue(()))
-            .notification::<notification::DidOpenTextDocument>(|_, _| ControlFlow::Continue(()))
-            .notification::<notification::DidChangeTextDocument>(|_, _| ControlFlow::Continue(()))
-            .notification::<notification::DidCloseTextDocument>(|_, _| ControlFlow::Continue(()))
+            .notification::<N::Initialized>(|_, _| std::ops::ControlFlow::Continue(()))
+            .notification::<N::DidChangeConfiguration>(|_, _| std::ops::ControlFlow::Continue(()))
+            .notification::<N::DidOpenTextDocument>(|_, _| std::ops::ControlFlow::Continue(()))
+            .notification::<N::DidChangeTextDocument>(|_, _| std::ops::ControlFlow::Continue(()))
+            .notification::<N::DidCloseTextDocument>(|_, _| std::ops::ControlFlow::Continue(()))
             .unhandled_notification(|_, notify| {
                 tracing::warn!("unhandled_notification `{}`", notify.method);
-                ControlFlow::Continue(())
+                std::ops::ControlFlow::Continue(())
             })
             .unhandled_request(|_, req| async move {
                 tracing::warn!("unhandled_request `{}`", req.method);
@@ -66,29 +55,30 @@ async fn main() {
                 ))
             });
 
-        ServiceBuilder::new()
-            .layer(TracingLayer::default())
-            .layer(LifecycleLayer::default())
-            .layer(CatchUnwindLayer::default())
-            .layer(ConcurrencyLayer::default())
-            .layer(ClientProcessMonitorLayer::new(client))
+        tower::ServiceBuilder::new()
+            .layer(async_lsp::tracing::TracingLayer::default())
+            .layer(async_lsp::server::LifecycleLayer::default())
+            .layer(async_lsp::panic::CatchUnwindLayer::default())
+            .layer(async_lsp::concurrency::ConcurrencyLayer::default())
+            .layer(async_lsp::client_monitor::ClientProcessMonitorLayer::new(
+                client,
+            ))
             .service(router)
     });
 
     tracing_subscriber::fmt()
-        .with_max_level(Level::INFO)
+        .with_max_level(tracing::Level::INFO)
         .with_ansi(false)
         .with_writer(std::io::stderr)
         .init();
 
-    // Prefer truly asynchronous piped stdin/stdout without blocking tasks.
-    #[cfg(unix)]
+    #[cfg(unix)] // Prefer truly asynchronous piped stdin/stdout without blocking tasks.
     let (stdin, stdout) = (
         async_lsp::stdio::PipeStdin::lock_tokio().unwrap(),
         async_lsp::stdio::PipeStdout::lock_tokio().unwrap(),
     );
-    // Fallback to spawn blocking read/write otherwise.
-    #[cfg(not(unix))]
+
+    #[cfg(not(unix))] // Fallback to spawn blocking read/write otherwise.
     let (stdin, stdout) = (
         tokio_util::compat::TokioAsyncReadCompatExt::compat(tokio::io::stdin()),
         tokio_util::compat::TokioAsyncWriteCompatExt::compat_write(tokio::io::stdout()),
